74491b32930fbee44baa808d0e9c732b9257d35d
ben-pollard-eedi <user@DESKTOP-IQQQTPT.>
2025-10-16 10:28:45 +0100

    fix: debug logging

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -25,7 +25,9 @@ az configure --defaults workspace="$aml_workspace" group="$resource_group"
 # Set variables for log storage
 traffic_type="$( [ "$traffic_percentage" -gt 0 ] && echo live || echo shadow )" # traffic type is used to direct logs of shadow traffic to a different queue/container
 endpoint_identity=$(az ml online-endpoint show --name $endpoint_name --query "identity.principal_id" -o tsv 2>/dev/null | sed 's/[[:space:]]//g' || true)
+echo "ℹ️ Endpoint identity: $endpoint_identity"
 storage_account_id=$(az storage account show --name $storage_account --query "id" -o tsv 2>/dev/null | sed 's/[[:space:]]//g' || true)
+echo "ℹ️ Storage account ID: $storage_account_id"
 queue_name="q-$(echo $endpoint_name | tr '[:upper:]' '[:lower:]')-$traffic_type"
 queue_id="${storage_account_id}/queueServices/default/queues/${queue_name}"
 container_name="blob-$queue_name"
